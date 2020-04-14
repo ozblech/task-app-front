fc969b9cac0aa1bc085153dc34bec58571f07d04
ozblech <ozblech87@gmail.com>
2020-04-14 21:39:39 +0300

    Updates

---
+++ precache-manifest.37f1d21ebfbae8295e34f48beda7b8ac.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09dcd28021aeb8458db253fb50b11372",
+    "url": "/task-app-front/index.html"
+  },
+  {
+    "revision": "ac442766c9dcdd0c5076",
+    "url": "/task-app-front/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "9b37dc863bd11fe1302a",
+    "url": "/task-app-front/static/css/main.5a8ab511.chunk.css"
+  },
+  {
+    "revision": "ac442766c9dcdd0c5076",
+    "url": "/task-app-front/static/js/2.f51b14a9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/task-app-front/static/js/2.f51b14a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b37dc863bd11fe1302a",
+    "url": "/task-app-front/static/js/main.3cb7b8cf.chunk.js"
+  },
+  {
+    "revision": "9a798aed100d8135a885",
+    "url": "/task-app-front/static/js/runtime-main.52bbfe7c.js"
+  },
+  {
+    "revision": "31cb6acd1ed68abe964c078310881d56",
+    "url": "/task-app-front/static/media/anonymous.31cb6acd.png"
+  }
+]);
