91c9f71689ce31be908e0fc50d3a1f55a653c666
Oz Blech <ozblech87@gmail.com>
2025-04-10 17:13:47 +0300

    trying to run in minikube

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 // For developement
 //const API_BASE_URL = 'http://localhost:3000';
 // For production
-const API_BASE_URL = 'http://api:3000';
+const API_BASE_URL = 'http://task-manager-api:3000';
 console.log("API URL:", API_BASE_URL);
 
 export { API_BASE_URL };
