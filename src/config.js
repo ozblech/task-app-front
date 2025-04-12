0d97767a050f44ba1cb67017f5829acb8dd04de3
Oz Blech <ozblech87@gmail.com>
2025-04-12 07:30:27 +0300

    trying to run in minikube using the backend service name

--- src/config.js
+++ src/config.js
@@ -1,7 +1,11 @@
 // For developement
 //const API_BASE_URL = 'http://localhost:3000';
-// For production
-const API_BASE_URL = 'http://task.local/api';
+
+// For production (minikube kubernetes)
+// const API_BASE_URL = 'http://task.local/api';
+
+// For production (minikube kubernetes)
+const API_BASE_URL = 'http://task-manager-api:3000';
 console.log("API URL:", API_BASE_URL);
 
 export { API_BASE_URL };
